557183f3f66dda67cdcb8978891f59653d45af99
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2017-09-30 12:07:47 +0900

    mod

---
+++ darknet/do.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MOVIE=$1
+if [[ $MOVIE = "" ]]; then
+  MOVIE=/home/work/takau/6.image/flow_detect/movie/bird
+fi
+
+DARKNET=./darknet
+DATA=cfg/combine9k.data
+NETWORK=cfg/yolo9000.cfg
+WEIGHTS=yolo9000.weights
+SOURCE=`basename $MOVIE`.mp4
+TARGET=out_`basename $MOVIE`.mp4
+
+rm -rf bbox_dump
+mkdir -p bbox_dump
+
+$DARKNET detector demo $DATA $NETWORK $WEIGHTS $MOVIE/$SOURCE
+
+ffmpeg -y -i out.avi $MOVIE/$TARGET
+rm -f out.avi
+
+mkdir -p $MOVIE
+rm -rf $MOVIE/bbox_dump
+mv -f bbox_dump $MOVIE
