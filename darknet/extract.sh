ed2f74cada1f8bc5e6dc69f91ff5e082f5566bda
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2019-07-09 10:25:26 +0900

    Updated deps

--- darknet/extract.sh
+++ darknet/extract.sh
@@ -2,26 +2,28 @@
 # wget http://pjreddie.com/media/files/yolo9000.weights
 
 TOPLEVEL=`git rev-parse --show-toplevel`
-MOVIE=$1
+MOVIE=$TOPLEVEL/$1
 if [[ $MOVIE = "" ]]; then
   MOVIE=$TOPLEVEL/movie/bird
 fi
 
-DARKNET=$TOPLEVEL/darknet/darknet
-DATA=$TOPLEVEL/darknet/cfg/combine9k.data
-NETWORK=$TOPLEVEL/darknet/cfg/yolo9000.cfg
-WEIGHTS=$TOPLEVEL/darknet/yolo9000.weights
+DARKNET=./darknet
+DATA=cfg/combine9k.data
+NETWORK=cfg/yolo9000.cfg
+WEIGHTS=yolo9000.weights
 SOURCE=`basename $MOVIE`.mp4
 TARGET=out_`basename $MOVIE`.mp4
 
-rm -rf bbox_dump
-mkdir -p bbox_dump
+(cd $TOPLEVEL/darknet;
+  rm -rf bbox_dump
+  mkdir -p bbox_dump
 
-$DARKNET detector demo $DATA $NETWORK $WEIGHTS $MOVIE/$SOURCE
+  $DARKNET detector demo $DATA $NETWORK $WEIGHTS $MOVIE/$SOURCE
 
-ffmpeg -y -i out.avi $MOVIE/$TARGET
-rm -f out.avi
+  ffmpeg -y -i out.avi $MOVIE/$TARGET
+  rm -f out.avi
 
-mkdir -p $MOVIE
-rm -rf $MOVIE/bbox_dump
-mv -f bbox_dump $MOVIE
+  rm -rf $MOVIE/bbox_dump
+  mkdir -p $MOVIE/bbox_dump
+  mv -f bbox_dump $MOVIE
+)
